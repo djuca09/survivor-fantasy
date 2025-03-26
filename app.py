69388570f1d597be40babec84034fdd98b32ad2c
djuca09 <danieljuca9@gmail.com>
2025-03-26 11:23:19 -0700

    prevent caching

--- app.py
+++ app.py
@@ -178,4 +178,11 @@ def admin_login():
             return redirect('/admin_login')
     return render_template('admin_login.html')
 
+@app.after_request
+def add_no_cache_headers(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "0"
+    return response
+
 app.run()
